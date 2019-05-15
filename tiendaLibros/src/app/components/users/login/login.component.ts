29543055ed30bbbe1ecbc9b97507ccad30ed6685
Erick Rodrigo Morales Severino <Erick.morales@correointerno.cl>
2019-05-15 16:32:05 -0400

    se cambian configuración de login al archivo service

--- tiendaLibros/src/app/components/users/login/login.component.ts
+++ tiendaLibros/src/app/components/users/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { auth } from 'firebase/app';
 import { Router } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -10,14 +11,18 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(public afAuth: AngularFireAuth, private router: Router) { }
+  constructor(public afAuth: AngularFireAuth, private router: Router, private authService: AuthService) { }
 
   ngOnInit() {
   }
 
-  onLoginGoogle(){
-    this.afAuth.auth.signInWithPopup( new auth.GoogleAuthProvider());
-    this.router.navigate(['admin/list-books']);
+  onLoginGoogle(): void {
+    this.authService.loginGoogleUser()
+    .then( (res) => {
+      console.log('resUser', res);
+      this.router.navigate(['admin/list-books']);
+    }).catch ( err => console.log('err', err));
+
   }
   onLoginFacebook(){
     this.afAuth.auth.signInWithPopup(new auth.FacebookAuthProvider());
--- tiendaLibros/src/app/services/auth.service.ts
+++ tiendaLibros/src/app/services/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import {map} from 'rxjs/operators';
+import { auth } from 'firebase/app';
+
 
 @Injectable({
   providedIn: 'root'
@@ -12,17 +14,22 @@ export class AuthService {
   registerUser(){
 
   }
-  loginEmailUser(){
+  loginEmailUser(email: string, pass: string) {
+    return new Promise((resolve, reject) => {
+      this.afsAuth.auth.signInWithEmailAndPassword(email, pass)
+      .then( userData => resolve(userData),
+      err => reject(err) );
+    });
 
   }
   loginFacebookUser(){
-
+   return this.afsAuth.auth.signInWithPopup(new auth.FacebookAuthProvider());
   }
   loginGoogleUser(){
-
+    return this.afsAuth.auth.signInWithPopup( new auth.GoogleAuthProvider());
   }
   logoutUser(){
-
+    return this.afsAuth.auth.signOut();
   }
   isAuth(){
     return this.afsAuth.authState.pipe(map(auth => auth));

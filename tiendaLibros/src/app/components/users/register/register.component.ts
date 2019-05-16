d1b8a8343c91ff7225f76e1430adb4abf1f59968
Erick Rodrigo Morales Severino <Erick.morales@correointerno.cl>
2019-05-16 13:30:05 -0400

    se agrega configuración para subir imagenes al registro de usuarios con firebase

--- tiendaLibros/src/app/components/users/register/register.component.ts
+++ tiendaLibros/src/app/components/users/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../services/auth.service';
 import { Router } from '@angular/router';
+import {AngularFireStorage } from '@angular/fire/storage';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -8,12 +9,23 @@ import { Router } from '@angular/router';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor(private router: Router, private authService: AuthService) { }
+  constructor(private router: Router, private authService: AuthService, private storage: AngularFireStorage) { }
 public email: string = '';
 public password: string = '';
   ngOnInit() {
   }
 
+  // metodo para cargar imagenes en firebase con libreria storage
+  onUpload(e){
+    // console.log('subir', e);
+    const id = Math.random().toString(36).substring(2);
+    const file = e.target.files[0];
+    const filePath = `uploads/profile_${id}`;
+    const ref = this.storage.ref(filePath);
+    const task = this.storage.upload(filePath, file);
+  }
+
+  // metodo para agregar usuario via registro con correo y contraseña
   onAddUser(){
     this.authService.registerUser(this.email, this.password)
     .then((res) => {
